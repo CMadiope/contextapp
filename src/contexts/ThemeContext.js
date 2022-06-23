ddae6e024cb1eda29a40e4178c10de96cfbc8646
Confidence Madiope <confidencem97@gmail.com>
2022-06-23 14:06:33 +0200

    ADD: themetoggle

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -3,18 +3,21 @@ import { createContext, Component } from "react";
 export const ThemeContext = createContext();
 
 class ThemeContextProvider extends Component {
-  state = { 
+  state = {
     isLightTheme: true,
-    light: {syntax: "#555", ui: "#ddd", bg: "#eee"},
-    dark:{syntax: '#ddd', ui: '#333', bg: '#555'}
-  } 
-  render() { 
+    light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+    dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+  };
+  toggleTheme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  };
+  render() {
     return (
-      <ThemeContext.Provider value={{...this.state}}>
+      <ThemeContext.Provider value={{ ...this.state,toggleTheme: this.toggleTheme }}>
         {this.props.children}
       </ThemeContext.Provider>
     );
   }
 }
- 
+
 export default ThemeContextProvider;
