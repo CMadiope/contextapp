4a7956b0ddabe249d2f1fe6b82e63c5517905858
Confidence Madiope <confidencem97@gmail.com>
2022-06-23 13:36:43 +0200

    ADD: booklist and navbar

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-const BookList = () => {
-  return (
-    <div>BookList</div>
-  )
+export default class BookList extends Component {
+  static contextType = ThemeContext;
+  render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <div className='bool-list' style={{color: theme.syntax, background: theme.bg}}>
+        <ul>
+          <li style = {{background: theme.ui}}>the way of the kings</li>
+          <li style = {{background: theme.ui}}>the name of the wind</li>
+          <li style = {{background: theme.ui}}>the final empire</li>
+        </ul>
+      </div>
+    );
+  }
 }
-
-export default BookList
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { Component } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
 
-const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+class Navbar extends Component {
+  static contextType = ThemeContext;
+  render() {
+    console.log(this.context);
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light : dark;
+    return (
+      <nav style={{background: theme.ui, color: theme.syntax}}>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>contact</li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
-export default Navbar
+export default Navbar;
